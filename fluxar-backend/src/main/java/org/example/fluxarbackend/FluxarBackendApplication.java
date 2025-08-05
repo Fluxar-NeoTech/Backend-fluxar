26a09ae1bffca6865da6a306f54a1bf424aff8da
heloisa-machado <heloisa.machado@germinare.org.br>
2025-08-05 08:20:37 -0300

    Remover .env do controle de versão e adicioná-lo ao .gitignore

---
+++ fluxar-backend/src/main/java/org/example/fluxarbackend/FluxarBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.fluxarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxarBackendApplication.class, args);
+    }
+
+}
