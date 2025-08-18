40b987245ee0cc17007ce2837fdd0471c202459a
heloisa-machado <heloisa.machado@germinare.org.br>
2025-08-18 07:29:06 -0300

    feat: criando projeto

---
+++ Backend-Fluxar/src/test/java/org/example/backendfluxar/BackendFluxarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendfluxar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFluxarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
