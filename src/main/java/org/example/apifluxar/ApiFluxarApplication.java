42781068c055410053cefcc36bd1ca5ad838e103
heloisa-machado <heloisa.machado@germinare.org.br>
2025-08-18 08:59:49 -0300

    origin/ feat/ projeto_configurado:colocando estrutura no novo projeto

---
+++ src/main/java/org/example/apifluxar/ApiFluxarApplication.java
@@ -0,0 +1,13 @@
+package org.example.apifluxar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFluxarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFluxarApplication.class, args);
+    }
+
+}
