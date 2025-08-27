ce98c44776aed45ad2cd6031e099b461360763b0
DudaCacciatore <mdoriacacciatore@gmail.com>
2025-08-27 17:12:19 -0300

    fix: removendo classes da tabela assinatura e arrumando imports de planService

--- src/main/java/org/example/apifluxar/repository/SubscriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.apifluxar.repository;
-
-import org.example.apifluxar.model.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-}
--- src/main/java/org/example/apifluxar/service/PlanService.java
+++ src/main/java/org/example/apifluxar/service/PlanService.java
@@ -2,14 +2,10 @@ package org.example.apifluxar.service;
 
 import org.example.apifluxar.dto.PlanResponseDTO;
 import org.example.apifluxar.model.Plan;
-import org.example.apifluxar.model.Subscription;
 import org.example.apifluxar.repository.PlanRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @Service
 public class PlanService {
