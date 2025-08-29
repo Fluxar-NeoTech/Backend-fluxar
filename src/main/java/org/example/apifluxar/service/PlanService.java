53c8ab252f58eb2ec296328250242516b427d5ad
DudaCacciatore <mdoriacacciatore@gmail.com>
2025-08-29 07:45:55 -0300

    fix: add objectMapper

--- src/main/java/org/example/apifluxar/service/PlanService.java
+++ src/main/java/org/example/apifluxar/service/PlanService.java
@@ -1,5 +1,6 @@
 package org.example.apifluxar.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.apifluxar.dto.PlanResponseDTO;
 import org.example.apifluxar.model.Plan;
 import org.example.apifluxar.repository.PlanRepository;
@@ -10,20 +11,15 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class PlanService {
     final PlanRepository planRepository;
+    final ObjectMapper objectMapper;
 
-    public PlanService(PlanRepository planRepository) {
+    public PlanService(PlanRepository planRepository, ObjectMapper objectMapper) {
         this.planRepository = planRepository;
-    }
-
-    public PlanResponseDTO fromPlanResponseDTO(Plan plan) {
-        PlanResponseDTO planResponseDTO = new PlanResponseDTO();
-        planResponseDTO.setNome(plan.getNome());
-        planResponseDTO.setDuracaoMeses(plan.getDuracaoMeses());
-        return planResponseDTO;
+        this.objectMapper = objectMapper;
     }
 
     public PlanResponseDTO getPlanById(Long id) {
         Plan plan = planRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        return fromPlanResponseDTO(plan);
+        return objectMapper.convertValue(plan, PlanResponseDTO.class);
     }
 }
